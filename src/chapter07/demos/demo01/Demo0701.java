4c6a8dcd932dfc8c45e9ff6da5bdd0475f11a787
SebastianDippold <82045881+SebastianDippold@users.noreply.github.com>
2021-04-21 15:47:13 +0200

    Test

---
+++ src/chapter07/demos/demo01/Demo0701.java
@@ -0,0 +1,42 @@
+package chapter07.demos.demo01;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX: Lebenszyklus einer JavaFX-Anwendung
+ * 
+ * @author Daniel Appenmaier
+ *
+ */
+public class Demo0701 extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void init() throws Exception {
+		// Eingabeparameter auslesen und verarbeiten
+		System.out.println(Messages.getString("Demo0701.0")); 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Aufbau der Bühne, Aufbau der Szene, Programmlogik
+		System.out.println(Messages.getString("Demo0701.1")); 
+		View view = new View();
+		Scene scene = new Scene(view.getRoot());
+		primaryStage.setTitle(Messages.getString("Demo0701.2")); 
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// "Aufräumarbeiten"
+		System.out.println(Messages.getString("Demo0701.3")); 
+	}
+
+}
